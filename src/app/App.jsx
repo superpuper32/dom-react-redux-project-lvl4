b8f67c4dab012ec61165ead8ff29fc44897c234c
superpuper32 <pochitalin_m@mail.ru>
2022-05-05 23:32:05 +0300

    sign up form added using formik yup & bootstrap

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,6 +4,7 @@ import {
   Route,
   Switch,
 } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 import Main from '../components/Main.jsx';
 import SignUp from '../components/SignUp.jsx';
@@ -11,21 +12,23 @@ import NoMatch from '../components/NoMatch.jsx';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Main />
-        </Route>
+    <Container className="p-3">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
 
-        <Route path="/login">
-          <SignUp />
-        </Route>
+          <Route path="/login">
+            <SignUp />
+          </Route>
 
-        <Route path="*">
-          <NoMatch />
-        </Route>
-      </Switch>
-    </Router>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
+    </Container>
   );
 }
 
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,9 +1,82 @@
 import React from 'react';
+import { useFormik } from 'formik';
+import { Container, Form, Button } from 'react-bootstrap';
+import * as Yup from 'yup';
+
+const passwordRegExp = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
+const initialValues = {
+  firstName: '',
+  password: '',
+};
+
+const onSubmit = (values) => {
+  alert(JSON.stringify(values, null, 2));
+};
+
+const validationSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Name is required'),
+  password: Yup.string()
+    .min(8, 'Must Contain 8 Characters')
+    .required('Please Enter your password')
+    .matches(
+      passwordRegExp,
+      'One Uppercase, One Lowercase, One Number and one special case Character',
+    ),
+});
 
 export default function SignUp() {
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validationSchema,
+  });
+
   return (
-    <section>
+    <Container className="p-5 mb-4 bg-light rounded-3">
       <h1>Sign Up</h1>
-    </section>
+      <Form onSubmit={formik.handleSubmit}>
+        <Form.Group className="mb-3" controlId="formFirstName">
+          <Form.Label>Name :</Form.Label>
+          <Form.Control
+            type="text"
+            name="firstName"
+            placeholder="Enter name"
+            autoComplete="username"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.firstName}
+            isInvalid={formik.touched.firstName && !!formik.errors.firstName}
+          />
+          <Form.Control.Feedback type="invalid">
+            {formik.errors.firstName}
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formPassword">
+          <Form.Label>Password :</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            autoComplete="current-password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+            isInvalid={formik.touched.password && !!formik.errors.password}
+          />
+          <Form.Control.Feedback type="invalid">
+            {formik.errors.password}
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Button type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
   );
 }
