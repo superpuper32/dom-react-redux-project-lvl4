54bfa536856c6a2f834fe7de7054d1c3e3e761a7
Misha <pochitalin.m@gmail.com>
2023-09-01 14:19:44 +0300

    step-2: formik & login form added

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import { Formik, Form, Field } from 'formik';
 
 export const Login = () => {
     return (
         <>
             <h1>Login</h1>
-            <p>text</p>
+            <Formik
+                initialValues={{ nickname: "", password: "" }}
+                onSubmit={({ setSubmitting }) => {
+                    console.log("Form is validated! Submitting the form...");
+                    setSubmitting(false);
+                }}
+                >
+                {() => (
+                    <Form>
+                        <div className="form-group">
+                            <label htmlFor="nickname">nickname</label>
+                            <Field
+                            type="nickname"
+                            name="nickname"
+                            className="form-control"
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <Field
+                            type="password"
+                            name="password"
+                            className="form-control"
+                            />
+                        </div>
+
+                        <button type="submit">Submit</button>
+                    </Form>
+                )}
+            </Formik>
         </>
     );
 };
